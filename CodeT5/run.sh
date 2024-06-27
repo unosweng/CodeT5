01b123d2da3802e0bf483ebd63e8ccd021d562ec
Your Name <you@example.com>
2024-06-27 11:56:16 -0500

    Updated at Thu Jun 27 11:56:16 AM CDT 2024

--- CodeT5/run.sh
+++ CodeT5/run.sh
@@ -13,15 +13,13 @@ nohup python run_gen.py > output-codet5-2024-$DATE.log 2>&1 &
 
 # ======================================================
 # Test Result with 1 epoch and 24 batch
-# 52 minutes with 1 GPU in selab2 (RTX 4090 24GB)
+# 52 mins with 1 GPU (RTX 4090 24GB) in selab2
 # ======================================================
 # (codet5) myoungkyu@oisit-selab2 √ ~/Documents/0-research-codet5/CodeT5 $ head -n 1 output-codet5-2024-06-27\=10-14pm.log 
 # 06/27/2024 10:56:05 - INFO - __main__ -   Namespace(do_train=True, do_eval=True, do_eval_bleu=True, do_test=True, task='summarize', sub_task='python', model_type='codet5', data_num=-1, num_train_epochs=1, warmup_steps=1000, learning_rate=5e-05, patience=2, tokenizer_name='Salesforce/codet5-base', model_name_or_path='Salesforce/codet5-base', data_dir='/home/myoungkyu@unomaha.edu/Documents/0-research-codet5/CodeT5/data', cache_path='saved_models/summarize/python/codet5_base_all_lr5_bs24_src256_trg128_pat2_e1/cache_data', output_dir='saved_models/summarize/python/codet5_base_all_lr5_bs24_src256_trg128_pat2_e1', summary_dir='tensorboard', save_last_checkpoints=True, always_save_model=True, res_dir='saved_models/summarize/python/codet5_base_all_lr5_bs24_src256_trg128_pat2_e1/prediction', res_fn='results/summarize_codet5_base.txt', train_batch_size=24, eval_batch_size=24, max_source_length=256, max_target_length=128, lang='python', eval_task='', add_lang_ids=False, start_epoch=0, add_task_prefix=False, load_model_path=None, train_filename=None, dev_filename=None, test_filename=None, config_name='', do_lower_case=False, no_cuda=False, gradient_accumulation_steps=1, beam_size=10, weight_decay=0.0, adam_epsilon=1e-08, max_grad_norm=1.0, save_steps=-1, log_steps=-1, max_steps=-1, eval_steps=-1, train_steps=-1, local_rank=-1, seed=1234)
-# (codet5) myoungkyu@oisit-selab2 √ ~/Documents/0-research-codet5/CodeT5 $ 
-# (codet5) myoungkyu@oisit-selab2 √ ~/Documents/0-research-codet5/CodeT5 $ tail -n 1 output-codet5-2024-06-27\=10-14pm.log 
-# 06/27/2024 11:48:50 - INFO - __main__ -   Finish and take 52m
-# (codet5) myoungkyu@oisit-selab2 √ ~/Documents/0-research-codet5/CodeT5 $ 
-# (codet5) myoungkyu@oisit-selab2 √ ~/Documents/0-research-codet5/CodeT5 $ 
+# ..
+# ..
+# ..
 # (codet5) myoungkyu@oisit-selab2 √ ~/Documents/0-research-codet5/CodeT5 $ tail -n 10 output-codet5-2024-06-27\=10-14pm.log 
 # 06/27/2024 11:42:10 - INFO - __main__ -     Num examples = 14918
 # 06/27/2024 11:42:10 - INFO - __main__ -     Batch size = 24
@@ -31,5 +29,4 @@ nohup python run_gen.py > output-codet5-2024-$DATE.log 2>&1 &
 # 06/27/2024 11:48:50 - INFO - __main__ -     bleu = 20.46
 # 06/27/2024 11:48:50 - INFO - __main__ -     em = 1.7831
 # 06/27/2024 11:48:50 - INFO - __main__ -   [best-bleu] bleu-4: 20.46, em: 1.7831, codebleu: 0.0000
-
 # 06/27/2024 11:48:50 - INFO - __main__ -   Finish and take 52m
